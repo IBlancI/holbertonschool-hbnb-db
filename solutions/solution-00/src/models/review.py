08c37514898baaefa6fa86cad54f4a0a46f8556c
IBlancI <adilslimani93130@gmail.com>
2024-07-08 02:14:46 +0200

    error of importation fixed

--- solutions/solution-00/src/models/review.py
+++ solutions/solution-00/src/models/review.py
@@ -2,36 +2,22 @@
 Review related functionality
 """
 
+from src import db
 from src.models.base import Base
-from src.models.place import Place
-from src.models.user import User
 
 
 class Review(Base):
-    """Review representation"""
+    __tablename__ = 'review'
 
-    place_id: str
-    user_id: str
-    comment: str
-    rating: float
+    place_id = db.Column(db.String(36), primary_key=True)
+    user_id = db.Column(db.String(36), primary_key=True)
+    comment = db.Column(db.String, nullable=True)
+    rating = db.Column(db.Float, nullable=False)
 
-    def __init__(
-        self, place_id: str, user_id: str, comment: str, rating: float, **kw
-    ) -> None:
-        """Dummy init"""
-        super().__init__(**kw)
-
-        self.place_id = place_id
-        self.user_id = user_id
-        self.comment = comment
-        self.rating = rating
-
-    def __repr__(self) -> str:
-        """Dummy repr"""
+    def __repr__(self):
         return f"<Review {self.id} - '{self.comment[:25]}...'>"
 
-    def to_dict(self) -> dict:
-        """Dictionary representation of the object"""
+    def to_dict(self):
         return {
             "id": self.id,
             "place_id": self.place_id,
@@ -43,32 +29,17 @@ class Review(Base):
         }
 
     @staticmethod
-    def create(data: dict) -> "Review":
+    def create(review_data):
         """Create a new review"""
-        from src.persistence import repo
-
-        user: User | None = User.get(data["user_id"])
-
-        if not user:
-            raise ValueError(f"User with ID {data['user_id']} not found")
-
-        place: Place | None = Place.get(data["place_id"])
-
-        if not place:
-            raise ValueError(f"Place with ID {data['place_id']} not found")
-
-        new_review = Review(**data)
-
-        repo.save(new_review)
-
+        new_review = Review(**review_data)
+        db.session.add(new_review)
+        db.session.commit()
         return new_review
 
     @staticmethod
-    def update(review_id: str, data: dict) -> "Review | None":
+    def update(review_id, data):
         """Update an existing review"""
-        from src.persistence import repo
-
-        review = Review.get(review_id)
+        review = Review.query.get(review_id)
 
         if not review:
             raise ValueError("Review not found")
@@ -76,6 +47,5 @@ class Review(Base):
         for key, value in data.items():
             setattr(review, key, value)
 
-        repo.update(review)
-
+        db.session.commit()
         return review
